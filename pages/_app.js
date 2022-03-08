a5e1255ebdc52604a6e42f1dbfa16d152cea4238
Abhinav Kallungal <abhinavkallungal15@gmail.com>
2022-03-08 09:21:58 +0530

    coming soon added

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,24 @@ import Header from '../components/Header';
 import { AuthProvider, useAuth } from '../hooks/useAuth';
 import '../styles/globals.css';
 import theme from '../theme';
-import { Box, Container, ThemeProvider } from '@material-ui/core';
+import { Box, Container, ThemeProvider, Typography } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
+import Counter from '../components/Counter'
 
 function MyApp({ Component, pageProps }) {
-  
+
   const router = useRouter();
   const state = useAuth();
-  
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
-    
+
     /*if(
         router.route && 
         (['/add', '/cart', '/checkout', '/f', 'thx'].some(i => {
@@ -32,25 +33,34 @@ function MyApp({ Component, pageProps }) {
         router.push('/signin');
       }
     }*/
-    
+
   }, [state, router]);
 
   return (
     <ThemeProvider theme={theme}>
-    <AuthProvider>
-     <Head/>
-     <div>
-       <div>
-          <Navbar/>
-        
-       </div>
-       
-       <Box >
+      <AuthProvider>
+        <Head />
+        <div>
+          <div style={{width:"100%",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center",textAlign:"center"}}> 
+          <div>
+
+          <Typography variant='h4' style={{fontWeight:"bold",  textShadow:" 2px 2px 4px #000000"}} >MAREBOX COMING SOON</Typography>
+          <Counter />
+          </div>
+          </div>
+          <div>
+           
+           
+            {/* <Navbar/> */}
+
+          </div>
+
+          {/* <Box >
           <Component {...pageProps}/>
           <Copyright/>
-       </Box>
-     </div>
-    </AuthProvider>
+       </Box> */}
+        </div>
+      </AuthProvider>
     </ThemeProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import Counter from '../components/Counter'
 
 const Home = () => {
 
@@ -44,84 +45,88 @@ const Home = () => {
 
   return (
     <div style={{ backgroundColor: `${state.themeBgColor}`, paddingTop: "0px" }}>
+      {
+        true ? <Counter/> : <>
+      
+      
 
-      <SearchComponent />
+        <SearchComponent />
+  
+        <Grid container
+          spacing={2}
+          style={{ padding: `15px` ,margin:"20px auto"}}
+        
+        > 
+  
+              <Typography className='title' variant="h3">Categories</Typography>
+            <Typography  variant="subtitle1" className='title-sub'  > Colors with Hex & RGB Codes has 6 colors </Typography>
+  
+  
+          {Object.values(homePageSections).map(homePageSection => (
+  
+            <Grid item xs={12} key={homePageSection.name} sm={6} md={3}>
+              <Link href={homePageSection.link}>
+                <Card className='category-card'>
+  
+                  <CardMedia
+  
+                    className='category-card-image'
+                    component="img"
+                    alt={homePageSection.name}
+                    height="150"
+                    image={`https://source.unsplash.com/featured/?${homePageSection.name} ${process.env.NEXT_PUBLIC_UNSPLASH_BANNER_TAG}`}
+                    title={homePageSection.name}
+                  />
+  
+                  <div className='category-card-content'>
+                    
+                    <Typography variant='h4'>{homePageSection.name}</Typography>
+                    <Typography> {homePageSection.description}</Typography>
+                  </div>
+                </Card>
+  
+  
+  
+               </Link>
+            </Grid>
+          ))}
+        </Grid>
+        {listings && (
+          <div style={{ padding: `50px` }} >
+            <Typography className='title' variant="h3">New Listings</Typography>
+            <Typography className='title-sub' variant="subtitle1"> Colors with Hex & RGB Codes has 6 colors </Typography>
+            <Listings listings={listings} />
+            <div style={{height:'100px' ,display:'flex',alignItems:"center",justifyContent:"center"}}>
+              <Button variant='contained' style={{backgroundColor:"#2E3E6F",color:"#ffffff",border:'5px solid #eee'}} endIcon={<ArrowForwardIos/>}>
+                  Checkout all listings
+              </Button>
+  
+            </div>
+          </div>
+        )}
+  
+        {items && (
+          <div style={{ padding: `50px`,  }}>
+         
+            <Typography className='title' variant="h3">New Items</Typography>
+            <Typography  variant="subtitle1" className='title-sub'> Colors with Hex & RGB Codes has 6 colors </Typography>
+            <Items items={items} />
+  
+            <div style={{height:'100px' ,display:'flex',alignItems:"center",justifyContent:"center"}}>
+              <Button variant='contained' style={{backgroundColor:"#2E3E6F",color:"#ffffff",border:'5px solid #eee'}} endIcon={<ArrowForwardIos/>}>
+                  Checkout all Items
+              </Button>
+  
+            </div>
+  
+          </div>
+        )}
+        </>
+      }
+
 
      
 
-      <Grid container
-        spacing={2}
-        style={{ padding: `15px` ,margin:"20px auto"}}
-      
-      > 
-
-            <Typography className='title' variant="h3">Categories</Typography>
-          <Typography  variant="subtitle1" className='title-sub'  > Colors with Hex & RGB Codes has 6 colors </Typography>
-
-
-        {Object.values(homePageSections).map(homePageSection => (
-
-          <Grid item xs={12} key={homePageSection.name} sm={6} md={3}>
-            <Link href={homePageSection.link}>
-              <Card className='category-card'>
-
-                <CardMedia
-
-                  className='category-card-image'
-                  component="img"
-                  alt={homePageSection.name}
-                  height="150"
-                  image={`https://source.unsplash.com/featured/?${homePageSection.name} ${process.env.NEXT_PUBLIC_UNSPLASH_BANNER_TAG}`}
-                  title={homePageSection.name}
-                />
-
-                <div className='category-card-content'>
-                  
-                  <Typography variant='h4'>{homePageSection.name}</Typography>
-                  <Typography> {homePageSection.description}</Typography>
-                </div>
-              </Card>
-
-
-
-             </Link>
-          </Grid>
-        ))}
-      </Grid>
-    
-
-
-
-      {listings && (
-        <div style={{ padding: `50px` }} >
-          <Typography className='title' variant="h3">New Listings</Typography>
-          <Typography className='title-sub' variant="subtitle1"> Colors with Hex & RGB Codes has 6 colors </Typography>
-          <Listings listings={listings} />
-          <div style={{height:'100px' ,display:'flex',alignItems:"center",justifyContent:"center"}}>
-            <Button variant='contained' style={{backgroundColor:"#2E3E6F",color:"#ffffff",border:'5px solid #eee'}} endIcon={<ArrowForwardIos/>}>
-                Checkout all listings
-            </Button>
-
-          </div>
-        </div>
-      )}
-
-      {items && (
-        <div style={{ padding: `50px`,  }}>
-       
-          <Typography className='title' variant="h3">New Items</Typography>
-          <Typography  variant="subtitle1" className='title-sub'> Colors with Hex & RGB Codes has 6 colors </Typography>
-          <Items items={items} />
-
-          <div style={{height:'100px' ,display:'flex',alignItems:"center",justifyContent:"center"}}>
-            <Button variant='contained' style={{backgroundColor:"#2E3E6F",color:"#ffffff",border:'5px solid #eee'}} endIcon={<ArrowForwardIos/>}>
-                Checkout all Items
-            </Button>
-
-          </div>
-
-        </div>
-      )}
     </div>
   )
 }
